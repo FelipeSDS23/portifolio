dceb626b1243b2e7bc8dad0c149ac181b2290581
FelipeSDS23 <felipe03san@gmail.com>
2023-09-28 16:56:12 -0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -17,7 +17,7 @@ const router = createBrowserRouter([
     element: <App/>,
     children: [
       {
-        path: "/portifolio/",
+        path: "/",
         element: <Inicio/>
       },
       {
